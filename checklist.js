ab6ef9cdce68508e614be4789ac6a5cb3485a2db
mohammedtakbir <takbirhossain678@gmail.com>
2022-08-27 09:23:23 +0600

    first commit

---
+++ checklist.js
@@ -0,0 +1,120 @@
+// * checklist 1
+// ? ১. কখন const আর কখন let দিয়ে ভেরিয়েবল ডিক্লেয়ার করতে হয় সেটা তোমাকে জানতেই হবে। তুমি নিজে নিজে একটা const দিয়ে আরেকটা let দিয়ে ভেরিয়েবল ডিক্লেয়ার করে ফেলো।
+const fixed = 'you cannot reassign the const value';
+let vary = 'you can reassign the let value'
+
+// * checklist 2
+// ? ২. টেম্পলেট স্ট্রিং দিয়ে একটা স্ট্রিং তৈরি করো। সেটার মধ্যে উপরে ডিক্লেয়ার করা ভেরিয়েবল এর মান ডাইনামিক ভাবে বসাও। একইভাবে উপরে একটা অবজেক্ট ডিক্লেয়ার করো। এবং ডাইনামিকভাবে উপরের অবজেক্ট এর দুইটা প্রপার্টি এর মান তোমার টেমপ্লেট স্ট্রিং এর মধ্যে বসাও। 
+
+let price = 10;
+const string = `the product price is ${price}`;
+// console.log(string);
+
+const object = {
+    id: 1,
+    product: 'Macbook',
+    price: 95000
+}
+const product = `i have a ${object.product} and the price of the Macbook is ${object.price}`
+// console.log(product)
+
+// * checklist 3
+//? ৩.১ একটা প্যারামিটার ওয়ালা arrow ফাংশন ডিক্লেয়ার করো। এবং সেই ফাংশনের কাজ হবে তুমি কোন ইনপুট দিলে সেই ইনপুট সংখ্যাকে ৫ দিয়ে ভাগ করে আউটপুট দিবে। 
+
+const divider = (input) => input / 5;
+const result = divider(100);
+// console.log(result)
+
+//? ৩.২ তুমি দুইটা প্যারামিটার ওয়ালা একটা অ্যারো ফাংশন লিখবে। সেই ফাংশনের ভিতরে কাজ হবে। প্রত্যেকটা ইনপুট প্যারামিটার এর সাথে ২ যোগ করবে তারপর যোগফল দুইটা গুণ করবে। ক্যামনে করবে সেটা চিন্তা করে বের করার চেষ্টা করো 
+
+const multiply = (a, b) => (a+2) * (b+2);
+// console.log(multiply(2, 2));
+
+//? ৩.৩ এইবার তিনটা প্যারামিটার ওয়ালা একটা অ্যারো ফাংশন ডিক্লেয়ার করো। যেই ফাংশনের কাজ হবে তিনটা প্যারামিটার নিয়ে সেই তিনটা প্যারামিটারকে গুণ করে সেই রেজাল্ট রিটার্ন করবে। 
+
+const multiplication = (a, b, c) => a * b * c;
+const result2 = multiplication(2, 2, 2);
+// console.log(result2)
+
+//? ৩.৪ এইবার দুইটা প্যারামিটার ওয়ালা একটা অ্যারো ফাংশন নিবে। ওই arrow ফাংশনটা হবে অনেকগুলা লাইনের। সেখানে প্রত্যেকটা ইনপুট প্যারামিটার এর সাথে ২ যোগ করবে তারপর যোগফল দুইটা গুণ করবে। ক্যামনে করবে সেটা চিন্তা করে বের করার চেষ্টা করো। 
+
+const getMultiply = (a, b) => {
+    const sum1 = a + 2;
+    const sum2 = b + 2;
+    const multiply = sum1 * sum2;
+    return multiply;
+}
+// console.log(getMultiply(2,2))
+
+// * checklist 4
+//? ৪. [হোম ওয়ার্ক] একটু গুগলে সার্চ দাও: javascript function declaration vs arrow function তারপর কয়েকটা আর্টিকেল পড়ে বুঝার চেষ্টা করো। 
+//? ৪.৫. জাভাস্ক্রিপ্ট এর var, let, const এর মধ্যে প্রার্থক্য কি? 
+
+// * checklist 5
+//? ৫. অনেকগুলা সংখ্যার একটা array হবে। তারপর তোমার কাজ হবে array এর উপরে map ইউজ করে। প্রত্যেকটা উপাদানকে ৫ দিয়ে গুন্ করে গুনফল আরেকটা array হিসেবে রাখবে। পুরা কাজটা এক লাইনে হবে। 
+
+const arr = [34, 45, 67, 78, 99, 12, 23, 43, 65, 76, 87]
+const multiplyByFive = arr.map(element => element * 5);
+// console.log(multiplyByFive);
+
+// * checklist 6
+//? ৬. [ চ্যালেঞ্জিং। গুগলে সার্চ দিয়ে বের করো ] অনেকগুলা সংখ্যার একটা array থেকে শুধু বিজোড় সংখ্যা বের করে নিয়ে আসার জন্য filter ইউজ করো 
+const arr1 = [34, 45, 67, 78, 99, 12, 23, 43, 65, 76, 87]
+const odd = arr1.filter(element => element % 2 !== 0);
+// console.log(odd)
+
+// * checklist 7
+//? ৭. একটা array এর মধ্যে অনেকগুলা অবজেক্ট আছে। সেখানে যেই অবজেক্ট এর price আছে ৫০০০ টেক্কা সেই অবজেক্টকে find দিয়ে বের করো। 
+const products = [
+    {product: 'walton', price: 12000},
+    {product: 'lava', price: 8000},
+    {product: 'maximus', price: 5000},
+    {product: 'symphony', price: 9000}
+]
+const find = products.find(product => product.price === 5000);
+// console.log(find);
+
+//? ৭.৫ [এক্সট্রা] জাভাস্ক্রিপ্ট এ array এর map, forEach, filter, find কোনটা দিয়ে কি হয়। সেটার একটা সামারি লিখে ফেলো। 
+
+// * checklist 8
+//? ৮. সিম্পল একটা জাভাস্ক্রিপ্ট অবজেক্ট এর কোন একটা প্রোপার্টিকে ভেরিয়েবল হিসেবে ডিক্লেয়ার করার জন্য destructuring ইউজ করো। 
+const {age} = {id: 21, name: 'takbir', age: 22, profession: 'programmer'};
+// console.log(age)
+
+// * checklist 9
+//? ৯. [চ্যালেঞ্জিং] array এর destructuring করবে আর সেটা করার জন্য তুমি এরে এর থার্ড পজিশন এর উপাদান কে destructuring করে 'three' নামক একটা ভেরিয়েবল এ রাখবে। 
+const arr2 = [34, 45, 67, 78, 99, 12, 23, 43, 65, 76, 87];
+const [, , three] = arr2;
+// console.log(three);
+
+// * checklist 10
+//? ১০. তিনটা প্যারামিটার ওয়ালা একটা ফাংশন লিখবে। যেই ফাংশনের কাজ হবে তিনটা প্যারামিটার নিয়ে সেই তিনটা প্যারামিটার এর যোগ করে যোগফল রিটার্ন করবে। আর থার্ড প্যারামিটার এর একটা ডিফল্ট ভ্যালু থাকবে। সেটা হবে ৭। 
+
+function getSum(a, b, c = 7){
+    const sum = a + b + c;
+    return sum
+}
+const result3 = getSum(2, 2);
+// console.log(result3)
+
+// * checklist 11
+//? ১১. একটা nested অবজেক্ট ডিক্লেয়ার করো (অর্থাৎ একটা অবজেক্ট এর প্রপার্টি এর মধ্যেও যে অবজেক্ট থাকতে পারে। আবার সেই অবজেক্ট এর প্রপার্টি এর মধ্যেও সে অবজেক্ট থাকতে পারে। সেই রকম একটা অবজেক্ট ডিক্লেয়ার করো। এবং যেকোন একটা প্রপার্টি এর মান একটা array হবে। জাস্ট এমন একটা অবজেক্ট )
+
+const myCars = {
+    brand: 'Porche',
+    model: 'GT4S',
+    detail: {
+        engine: {
+            selinder: '6',
+            turbo: 'nope',
+            gearbox: 'dual clutch auto'
+        },
+        driveSystem: 'rear wheel drive'
+    },
+    year: [2010, 2018, 2021, 2025]
+}
+
+// * checklist 12
+//? ১২. উপরের অবজেক্ট এ ডট এর আগে যে প্রশ্নবোধক চিহ্ন দিয়ে যে অপশনাল চেইনিং করা যায়। সেটা একটু প্রাকটিস করো। 
+const findGearBox = myCars.detail.engin?.gearbox ?? "cannot find the engine!";
+// console.log(findGearBox);
---
+++ recap.js
@@ -0,0 +1,31 @@
+// * must know
+// ? 1. var, let, const
+// ? 2. template string
+    // ? 2.1---> create multiline string 
+    // ? 2.2---> add dynamic varaible (expression): `${}`  
+
+// ? 3.arrow function
+    // ? 3.1----> single parameter arrow function 
+    // ? 3.2----> two parameter arrow function 
+    // ? 3.3----> no parameter arrow function 
+    // ? 3.4----> arrow function with functin body. And don't forget to return the body explicitly
+
+// ? 4. map, filter, find
+// ? 5. optional chaining
+// * good to have
+// ? 1. default parameter 
+// ? 2. spread operator(...spread)---> [798, 634,...numbers, 34, 45] 
+// ? 3. destructuring
+    // ? 2.1---> object destructuring--->how to get property name as variable
+        // ? const {x} = {x:5, y:7}
+    // ? 2.1---> array destructuring--->
+        // ? const [a,b] = [3, 45, 6, 8] 
+
+// ? 4. forEach
+// ? 5. dot notaion vs bracket notaion
+
+// * should know the name
+// ? 1. reduce()
+// ? 2. inheritence
+// ? 3. prototypical chain
+// ? 5. class, object
